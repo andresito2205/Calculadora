deb3cb6b307a7f8b7b8b898376808ade44411b75
Victoria Gallego Eastmond <vgallegoe@sura.com.co>
2022-01-20 11:04:51 -0500

    VGE - Se agrega proyecto Calculadora, se agrega operacion suma al proyecto Calculadora

---
+++ src/MiCalculadora.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+
+
+import javax.swing.JOptionPane;
+
+
+
+public class MiCalculadora {
+
+
+
+public static void main(String[]arg) {
+
+
+
+int numero1 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese numero1"));
+int numero2 = Integer.parseInt(JOptionPane.showInputDialog("Ingrese numero2"));
+
+
+
+
+
+JOptionPane.showMessageDialog(null, "El resultado de la suma es: " +(numero1+numero2));
+
+
+
+}
+}
